fa2c50118f613ac21ca72ebe245978fa90026d63
Renzo Sala <renzo.sala@cardioscan.de>
2026-04-14 13:59:36 +0900

    Data modeling update

--- scripts/compiler.py
+++ scripts/compiler.py
@@ -1,12 +1,20 @@
 import spacy
+from spacy import displacy
 
-# can get the language
-
+# example
+s1_it = "Siccome il cane bianco e nero che sono andato a prendere ieri si è rivelato essere un cane molto simpatico e intelligente, e mia moglie mari lo ha adorato dal primo momento che lo ha visto, pensiamo che adottarlo sia la prossima mossa"
+s1_en = "Since the black-and-white dog I went to pick up yesterday turned out to be a very friendly and intelligent dog, and my wife Mari loved him from the moment she saw him, we think adopting him is the next step"
 
 # English pipelines include a rule-based lemmatizer
 nlp = spacy.load("en_core_web_md")
 lemmatizer = nlp.get_pipe("lemmatizer")
-doc = nlp("While I was reading the paper, I found a great idea.")
+doc = nlp(s1_en)
 
 for token in doc:
-    print(f"{token.text} -> {token.lemma_}")
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+
+
+# displacy.serve(doc, style='ent', port=3000)
+# displacy.serve(doc, style='dep', port=3001)
+# displacy.serve(doc, style='parse', port=3002)
--- tokenKino/lib/core/entities.py
+++ tokenKino/lib/core/entities.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from typing import List, Optional, Union, Literal
+from enum import Enum
 from pydantic import BaseModel, Field
 
 # geo point
@@ -31,7 +33,64 @@ class TKPlace(BaseModel):
     category: str
     parent_admin: str
     parent_geo: str
-    path_admin: List[str]
-    path_geo: List[str]
+    path_admin: List[str] = Field(default_factory=list)
+    path_geo: List[str] = Field(default_factory=list)
     physical_features: Optional[List[str]] = None
     location: Optional[GeoPoint] = None
+
+# --------------------------------------------------
+# statements
+# --------------------------------------------------
+
+# es testvar: TKOperator = TKOperator.AND
+# operator enum
+class TKOperator(str, Enum):
+    NOT = "NOT"
+    AND = "AND"
+    OR = "OR"
+    XOR = "XOR"
+    XNOR = "XNOR"
+    IMPLY = "IMPLY"
+    CONV = "CONV"
+    EQ = "EQ"
+
+# entities involved in statements
+class TKEntity(BaseModel):
+    id: int # unique identifier for the entity IN THE CONTEXT OF THE STATEMENT
+    type: str
+    name: Optional[TKName] = None
+    dictionary: Optional[TKDictionary] = None
+    place: Optional[TKPlace] = None
+    statement: Optional[TKStatement] = None
+
+# LL statement
+class TKStatement(BaseModel):
+    op: Optional[TKOperator] = None
+    subject: TKEntity
+    predicate: TKEntity 
+    object: Optional[TKEntity] = None
+    when: Optional[TKEntity] = None
+    where: Optional[TKEntity] = None
+    spec: Optional[TKEntity] = None
+    entities: List[TKEntity] = Field(default_factory=list)
+
+class TKLLStatement(List[TKStatement]):
+    pass
+
+# rebuild models to ensure all fields are properly processed
+TKEntity.model_rebuild()
+TKStatement.model_rebuild()
+
+# Note per sviluppo delle logiche di valutazione
+# - subject, predicate, object e spec si valutano sempre su base semantica
+# - when, si valuta su base temporale costruendo una linea temporale degli eventi
+# - where, si valuta su base spaziale costruendo una mappa degli eventi
+# - la similarity semantica dà valori tra -1 e 1, prima dell'applicazione degli operatori
+#   logici, va normalizzata tra 0 e 1, ad esempio con la formula (similarity + 1) / 2 
+# - LLC appiattisce le relazioni logiche tra le entità, ad esempio una relazione AND tra due affermazioni diventa una nuova affermazione con un nuovo soggetto che rappresenta la combinazione dei due soggetti originali, e così via per gli altri operatori logici
+#   es: io e mari andiamo al mare ->
+#       [io, subject] [andare, predicate] [mare, object] [con mari, spec]
+#       AND
+#       [mari, subject] [andare, predicate] [mare, object] [con io, spec]
+#   In questo modo le valutazioni semantiche sono SOLO semantiche (senza mischiare arbitrariamente i valori
+#   semantici, mentre quelle logiche (fuzzy) sono solo fuzzy
